d780498f1797f3d15c1443760983a103e6ce3f51
JamesGabby <jlghfe36@gmail.com>
2025-08-28 13:03:43 +0700

    nav bar and styling changes

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,14 @@ import type { Metadata } from "next";
 import { Geist } from "next/font/google";
 import { ThemeProvider } from "next-themes";
 import "./globals.css";
+import { hasEnvVars } from "@/lib/utils";
+import Link from "next/link";
+import { AuthButton } from "@/components/auth-button";
+import { EnvVarWarning } from "@/components/env-var-warning";
+import { Sansation } from "next/font/google";
+
+const sansation = Sansation({ weight: '700', subsets: ['latin'], fallback: ['mono']});
+
 
 const defaultUrl = process.env.VERCEL_URL
   ? `https://${process.env.VERCEL_URL}`
@@ -9,8 +17,8 @@ const defaultUrl = process.env.VERCEL_URL
 
 export const metadata: Metadata = {
   metadataBase: new URL(defaultUrl),
-  title: "Next.js and Supabase Starter Kit",
-  description: "The fastest way to build apps with Next.js and Supabase",
+  title: "Know Real - Meditation, Lucid Dreaming and Out of Body Experiences",
+  description: "The place to go for meaning and knowledge.",
 };
 
 const geistSans = Geist({
@@ -27,6 +35,14 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <body className={`${geistSans.className} antialiased`}>
+        <nav className="w-full flex justify-center border-b border-b-foreground/10 h-16">
+          <div className="w-full max-w-5xl flex justify-between items-center p-3 px-5 text-sm">
+            <div className="flex gap-5 items-center font-semibold">
+              <Link href={"/"} className={`${sansation.className}`}>KNOW REAL</Link>
+            </div>
+            {!hasEnvVars ? <EnvVarWarning /> : <AuthButton />}
+          </div>
+        </nav>
         <ThemeProvider
           attribute="class"
           defaultTheme="system"
--- components/hero.tsx
+++ components/hero.tsx
@@ -1,27 +1,13 @@
-import { NextLogo } from "./next-logo";
-import { SupabaseLogo } from "./supabase-logo";
 import { Sansation } from "next/font/google";
 
-const sansation = Sansation({ weight: '400', subsets: ['latin'], fallback: ['mono'] });
+const sansation = Sansation({ weight: '700', subsets: ['latin'], fallback: ['mono'] });
 
 export function Hero() {
   return (
     <div className="flex flex-col gap-16 items-center">
       <div className="flex gap-8 justify-center items-center">
-        {/* <a
-          href="https://supabase.com/?utm_source=create-next-app&utm_medium=template&utm_term=nextjs"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <SupabaseLogo />
-        </a>
-        <span className="border-l rotate-45 h-6" />
-        <a href="https://nextjs.org/" target="_blank" rel="noreferrer">
-          <NextLogo />
-        </a> */}
         <h1 className={`${sansation.className} text-5xl font-mono`}>KNOW REAL</h1>
       </div>
-      <h1 className="sr-only">Supabase and Next.js Starter Template</h1>
       <p className="text-xl lg:text-2xl !leading-tight mx-auto max-w-xl text-center">
         Get to know{" "}
         <span className="font-bold">
